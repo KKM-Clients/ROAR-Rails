30a231efdc7ed0b77a12c0411438cc9e0bac4f13
Kfn8dkodemonkey <kaffeinatedkodemonkey@gmail.com>
2018-06-27 20:47:29 -0600

    working on removing error now

--- app/assets/javascripts/sqpaymentform.js
+++ app/assets/javascripts/sqpaymentform.js
@@ -152,7 +152,7 @@ var paymentForm = new SqPaymentForm({
         return;
       }
 
-      alert('Nonce received: ' + nonce); /* FOR TESTING ONLY */
+      //alert('Nonce received: ' + nonce); /* FOR TESTING ONLY */
 
       // Assign the nonce value to the hidden form field
       document.getElementById('card-nonce').value = nonce;
