a73579100d2dcb0a8e193d41947063e5285a350e
Kfn8dkodemonkey <kaffeinatedkodemonkey@gmail.com>
2018-12-08 20:11:11 -0700

    add snipcart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   $csp_closed = "2018-07-15"
   $rc_closed = "2018-07-22"
-  $roar_closed = "2018-08-12"
+  $roar_closed = "2019-05-01"
   $cphc_closed = "2018-08-9"
 end
